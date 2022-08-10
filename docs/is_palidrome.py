f0a7b244de945b6c615c5e89764920f1dd33c137
Dave Pitts <dgapitts@gmail.com>
2022-08-10 21:49:29 +0200

    is_palidrome made more efficient and function

--- docs/is_palidrome.py
+++ docs/is_palidrome.py
@@ -1,22 +1,17 @@
 import re
-test="1'2!3?21"
-#test="1232"
-#test="12320"
-test="12 3 32 1"
-# https://stackoverflow.com/questions/5843518/remove-all-special-characters-punctuation-and-spaces-from-string
-# https://stackoverflow.com/questions/6797984/how-do-i-lowercase-a-string-in-python
-test=re.sub('[^A-Za-z0-9]+', '',test).lower()
-print(test)
-is_palidrome=True
-for i in range(0,round((len(test)-1)/2)+1):
-  print(str("round: ")+str(i))
-  print(str(i)+","+str((len(test)-1)-i))
-  print(test[i]+","+test[(len(test)-1)-i])
-  if test[i] != test[(len(test)-1)-i]:
-    is_palidrome=False
-    print(str(i) + " False")
+def is_palidrome (test):
+  test=re.sub('[^A-Za-z0-9]+', '',test).lower()
+  for i in range(0,round((len(test)-1)/2)+1):
+    if test[i] != test[(len(test)-1)-i]:
+      return False
+  return True
+
+#input_str="1'2!3?4321"
+#input_str="1232"
+#input_str="12320"
+input_str="12 3 32 1"
+print(str(input_str)+": "+str(is_palidrome(input_str)))
 
 
-print (test + " " + str(is_palidrome))
 
   
